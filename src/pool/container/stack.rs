e2e55f780a4bf5f2aa3618296b11db24f0ba094c
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2023-03-22 20:04:43 +0000

    Work on StackPool trait

--- src/pool/container/stack.rs
+++ src/pool/container/stack.rs
@@ -7,91 +7,152 @@ use crate::forward_pool_traits;
 use super::*;
 
 /// A [`Pool`] allocating stacks containing elements of type `Self::Item`
-pub trait StackPool<K>: ContainerPool<K> {
-    /// Allocate a new, empty list with the given capacity
+pub trait StackPool<K>: ContainerPool<K> + InsertEmpty<K> {
+    /// Allocate a new, empty stack with the given capacity
     ///
     /// Note that the capacity is *not* guaranteed.
     ///
     /// Return an error on failure
     ///
     /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
+    #[must_use]
     fn new_with_capacity(&mut self, capacity: usize) -> Result<K, ()>;
 
-    /// Push an element to a list
+    /// Push an element to a stack
     ///
-    /// On success, returns the list's key, which may have been changed (in this case, the old key should be considered deleted).
+    /// On success, returns the stack's key, which may have been changed (in this case, the old key should be considered deleted).
     /// On failure, panics
     ///
     /// Fails if:
     /// - The pool is out of capacity
     ///
     /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
-    fn push(&mut self, key: K, item: Self::Elem) -> K {
-        self.try_push(key, item)
+    #[cfg_attr(not(tarpaulin), inline(always))]
+    #[must_use]
+    fn into_pushed(&mut self, key: K, item: Self::Elem) -> K {
+        self.try_into_pushed(key, item)
             .ok()
-            .expect("failed to push to list")
+            .expect("failed to move-push to stack")
     }
 
-    /// Pop an element from a list
+    /// Pop an element from a stack, returning a (potentially new) key for the stack as well as the popped value.
     ///
-    /// On success, returns the poppsed value and the list's key, which may have changed.
-    /// When called on an empty list, returns `None`, leaving the list unchanged.
+    /// Returns `None` and leaves the stack unchanged given a key for an empty stack.
+    /// Otherwise, returns the old value and the new key; the old key (if different from the new key) should be considered deleted.
+    /// Panics on failure.
+    ///
+    /// Fails if:
+    /// - The pool is out of capacity, and moving the stack would require an allocation
     ///
     /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
-    fn pop(&mut self, key: K) -> Option<(K, Self::Elem)>;
+    #[cfg_attr(not(tarpaulin), inline(always))]
+    #[must_use]
+    fn into_popped(&mut self, key: K) -> Option<(K, Self::Elem)> {
+        self.try_into_popped(key)
+            .expect("failed to move-pop from stack")
+    }
 
-    /// Try to push an element to a list
+    /// Try to pop an element from a stack, returning a (potentially new) key for the stack as well as the popped value.
     ///
-    /// On success, returns the list's key, which may have been changed (in this case, the old key should be considered deleted).
-    /// On failure, returns the item, leaving the list unchanged.
+    /// Returns `None` and leaves the stack unchanged given a key for an empty stack.
+    /// Otherwise, returns the old value and the new key; the old key (if different from the new key) should be considered deleted.
+    ///
+    /// Fails if:
+    /// - The pool is out of capacity, and moving the stack would require an allocation
+    ///
+    /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
+    fn try_into_popped(&mut self, key: K) -> Result<Option<(K, Self::Elem)>, ()>;
+
+    /// Try to push an element to a stack, returning a (potentially new) key for the stack as well as the popped value
+    ///
+    /// On success, returns the stack's key, which may have been changed (in this case, the old key should be considered deleted).
+    /// On failure, returns the item, leaving the stack unchanged.
     ///
     /// Fails if:
     /// - The pool is out of capacity
     ///
     /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
-    fn try_push(&mut self, key: K, item: Self::Elem) -> Result<K, Self::Elem>;
+    fn try_into_pushed(&mut self, key: K, item: Self::Elem) -> Result<K, Self::Elem>;
 
-    /// Pop an element from a list without moving it
+    /// Pop an element from a stack
     ///
-    /// On success, returns the poppsed value and the list's key, which may have changed.
-    /// When called on an empty list, returns `Ok(None)`, leaving the list unchanged.
+    /// On success, returns the popped value.
+    /// When called on an empty stack, returns `None`, leaving the stack unchanged.
+    /// Panics on failure.
+    ///
+    /// Fails if:
+    /// - Popping an element to the stack would require moving the stack
+    ///
+    /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
+    #[cfg_attr(not(tarpaulin), inline(always))]
+    fn pop(&mut self, key: K) -> Option<Self::Elem> {
+        self.try_pop(key).expect("failed to pop from stack")
+    }
+
+    /// Push an element to a stack
+    ///
+    /// Panics on failure
+    ///
+    /// Fails if:
+    /// - The pool is out of capacity
+    /// - Pushing an element to the stack would require moving the stack
+    ///
+    /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
+    #[cfg_attr(not(tarpaulin), inline(always))]
+    fn push(&mut self, key: K, item: Self::Elem) {
+        self.try_push(key, item)
+            .ok()
+            .expect("failed to push from stack")
+    }
+
+    /// Try to pop an element from a stack
+    ///
+    /// On success, returns the popped value.
+    /// When called on an empty stack, returns `Ok(None)`, leaving the stack unchanged.
     /// On failure, returns `Err(())`.
     ///
-    /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
-    fn pop_pinned(&mut self, key: K) -> Result<Option<Self::Elem>, ()>;
-
-    /// Try to push an element to a list without moving it
+    /// Fails if:
+    /// - Popping an element to the stack would require moving the stack
     ///
-    /// On failure, returns the item, leaving the list unchanged
+    /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
+    fn try_pop(&mut self, key: K) -> Result<Option<Self::Elem>, ()>;
+
+    /// Try to push an element to a stack
+    ///
+    /// On failure, returns the item, leaving the stack unchanged
     ///
     /// Fails if:
-    /// - Pushing an element to the list would move the list
     /// - The pool is out of capacity
+    /// - Pushing an element to the stack would require moving the stack
     ///
     /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
-    fn push_pinned(&mut self, key: K, item: Self::Elem) -> Result<(), Self::Elem>;
+    fn try_push(&mut self, key: K, item: Self::Elem) -> Result<(), Self::Elem>;
 
-    /// Get the capacity of the list corresponding to the provided key
+    /// Get the capacity of the stack corresponding to the provided key
     ///
-    /// The result is guaranteed to be greater than the length of the list.
-    /// If a number greater than the length is returned, then it is guaranteed that pushing up to this number of elements to the list will not move it.
+    /// If a number greater than the length is returned, then it is guaranteed that pushing up to this number of elements to the stack will always succeed.
+    /// If a number less than or equal to the length is returned, then no guarantees are made; in particular, 0 is always a safe return value.
     ///
     /// Returns an unspecified value or panics if used on an unrecognized key.
-    fn capacity(&self, key: K) -> usize;
+    #[cfg_attr(not(tarpaulin), inline(always))]
+    #[must_use]
+    fn capacity(&self, _key: K) -> usize {
+        0
+    }
 
-    /// Clear the provided list, returning the key to an empty list
+    /// Clear the provided stack, returning the key to an empty stack
     ///
-    /// In some implementations, the returned key will preserve the capacity of the input list, but this is *not* guaranteed.
+    /// In some implementations, the returned key will preserve the capacity of the input stack, but this is *not* guaranteed.
     ///
     /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
     fn clear(&mut self, key: K) -> K;
 
-    /// Try to clear the provided list without moving it
+    /// Try to clear the provided stack without moving it
     ///
-    /// On failure, returns an error, leaving the list unchanged.
-    /// Note that this method is allowed to fail where `pop_pinned` in a loop might succeed, since the latter does *not* leave the list unchanged on failure!
+    /// On failure, returns an error, leaving the stack unchanged.
+    /// Note that this method is allowed to fail where `pop_pinned` in a loop might succeed, since the latter does *not* leave the stack unchanged on failure!
     ///
-    /// In some implementations, the capacity of the input list will be preserved, but this is *not* guaranteed.
+    /// In some implementations, the capacity of the input stack will be preserved, but this is *not* guaranteed.
     ///
     /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
     fn clear_pinned(&mut self, key: K) -> Result<(), ()>;
@@ -110,7 +171,7 @@ pub trait ContainerLike {
 
 /// A trait implemented by things which can be pushed to and popped to like a stack
 pub trait StackLike: ContainerLike + Default {
-    /// Allocate a new, empty list with the given capacity
+    /// Allocate a new, empty stack with the given capacity
     ///
     /// Note that the capacity is *not* guaranteed.
     ///
@@ -125,21 +186,21 @@ pub trait StackLike: ContainerLike + Default {
     /// - The stack is out of capacity and more cannot be allocated
     fn push_stack(&mut self, item: Self::Elem);
 
-    /// Pop an element from a list
+    /// Pop an element from a stack
     ///
     /// On success, returns the popped value
-    /// When called on an empty list, returns `None`, leaving the list unchanged.
+    /// When called on an empty stack, returns `None`, leaving the stack unchanged.
     ///
     /// Leaves the pool in an unspecified state and returns an unspecified value or panics if used on an unrecognized key
     fn pop_stack(&mut self) -> Option<Self::Elem>;
 
-    /// Try to push an element to a list
+    /// Try to push an element to a stack
     ///
     /// On success, returns `Ok(())`
-    /// On failure, returns the item, leaving the list unchanged.
+    /// On failure, returns the item, leaving the stack unchanged.
     ///
     /// Fails if:
-    /// - The list is out of capacity and more cannot be allocated
+    /// - The stack is out of capacity and more cannot be allocated
     fn try_push_stack(&mut self, item: Self::Elem) -> Result<(), Self::Elem>;
 
     /// Get the capacity of this stack
@@ -202,6 +263,17 @@ where
     type Elem = <P::Value as ContainerLike>::Elem;
 }
 
+impl<P, K> InsertEmpty<K> for ContainerLikePool<P>
+where
+    P: InsertPool<K> + PoolMut<K> + PoolRef<K>,
+    K: Clone,
+    P::Value: StackLike,
+{
+    fn insert_empty(&mut self) -> Result<K, ()> {
+        self.new_with_capacity(0)
+    }
+}
+
 impl<P, K> StackPool<K> for ContainerLikePool<P>
 where
     P: InsertPool<K> + PoolMut<K> + PoolRef<K>,
@@ -215,12 +287,12 @@ where
     }
 
     #[cfg_attr(not(tarpaulin), inline(always))]
-    fn pop(&mut self, key: K) -> Option<(K, Self::Elem)> {
-        self.0.get_mut(key.clone()).pop_stack().map(|v| (key, v))
+    fn try_into_popped(&mut self, key: K) -> Result<Option<(K, Self::Elem)>, ()> {
+        Ok(self.0.get_mut(key.clone()).pop_stack().map(|v| (key, v)))
     }
 
     #[cfg_attr(not(tarpaulin), inline(always))]
-    fn try_push(&mut self, key: K, item: Self::Elem) -> Result<K, Self::Elem> {
+    fn try_into_pushed(&mut self, key: K, item: Self::Elem) -> Result<K, Self::Elem> {
         self.0
             .get_mut(key.clone())
             .try_push_stack(item)
@@ -228,12 +300,12 @@ where
     }
 
     #[cfg_attr(not(tarpaulin), inline(always))]
-    fn pop_pinned(&mut self, key: K) -> Result<Option<Self::Elem>, ()> {
+    fn try_pop(&mut self, key: K) -> Result<Option<Self::Elem>, ()> {
         Ok(self.0.get_mut(key).pop_stack())
     }
 
     #[cfg_attr(not(tarpaulin), inline(always))]
-    fn push_pinned(&mut self, key: K, item: Self::Elem) -> Result<(), Self::Elem> {
+    fn try_push(&mut self, key: K, item: Self::Elem) -> Result<(), Self::Elem> {
         self.0.get_mut(key).try_push_stack(item)
     }
 
